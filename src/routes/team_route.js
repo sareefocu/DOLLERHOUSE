bb228f74ec00295ead599a4df3638d7aa15e91f2
sareefocu <158563957+sareefocu@users.noreply.github.com>
2024-04-02 18:45:30 +0530

    Update team_route.js

--- src/routes/team_route.js
+++ src/routes/team_route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getSingleMember, getTeamController, lastModelController, getalldata } from "../controllers/lastModel_controller.js";
+import { getSingleMember, getTeamController, lastModelController, getalldata, setdata11 } from "../controllers/lastModel_controller.js";
 let teamRouter = Router();
 teamRouter.post('/add', lastModelController);
 teamRouter.get('/get', getTeamController)
 teamRouter.get('/single-member', getSingleMember)
 teamRouter.get('/leval5-member/:id/:leval/:planPrice', getalldata)
+teamRouter.get('/leval5-membe22/:id', setdata11)
 export default teamRouter;
