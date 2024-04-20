371ff5f692c311b0abc50a089afc43b8464a4103
sareefocu <158563957+sareefocu@users.noreply.github.com>
2024-04-20 12:29:10 +0530

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,6 @@
 let constants = {
-  connectionUrl:
-    "mongodb+srv://Shahrukh:Ansari12311@cluster0.uiuq3hd.mongodb.net/newWeb3db2",
-  app_name: "newWeb3project",
-};
+    connectionUrl:'mongodb+srv://Shahrukh:Ansari12311@cluster0.uiuq3hd.mongodb.net/newWeb3db23',
+    app_name:"newWeb3project",
+}
 
-export default constants;
-
-
+export default constants ;
